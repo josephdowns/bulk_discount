ef952d4f8a3b3ba1fd62302f3810fb66e12268cc
Joseph Downs <jos.w.downs@gmail.com>
2022-04-26 13:07:17 -0600

    Get coverage to 100%

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -8,8 +8,64 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of :item_id }
     it { should validate_presence_of :invoice_id }
   end
+
   describe "relationships" do
      it { should belong_to :item }
      it { should belong_to :invoice }
+  end
+
+  describe "methods" do
+    it "finds discounts" do
+      @merchant1 = Merchant.create!(name: "Pabu")
+
+      @discount1 = @merchant1.discounts.create!(discount: 0.1, threshold: 5)
+      @discount2 = @merchant1.discounts.create!(discount: 0.5, threshold: 20)
+
+      @item1 = Item.create!(name: "Brush", description: "Brushy", unit_price: 10, merchant_id: @merchant1.id)
+      @item2 = Item.create!(name: "Peanut Butter", description: "Yummy", unit_price: 12, merchant_id: @merchant1.id)
+
+      @customer1 = Customer.create!(first_name: "Loki", last_name: "R")
+
+      @invoice1 = @customer1.invoices.create!(status: "completed")
+
+      @ii1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, status: 1, quantity: 20, unit_price: 1000)
+
+      expect(@ii1.applied).to eq(@discount2)
+    end
+
+    it "returns true if there is a discount" do
+      @merchant1 = Merchant.create!(name: "Pabu")
+
+      @discount1 = @merchant1.discounts.create!(discount: 0.1, threshold: 5)
+      @discount2 = @merchant1.discounts.create!(discount: 0.4, threshold: 20)
+
+      @item1 = Item.create!(name: "Brush", description: "Brushy", unit_price: 10, merchant_id: @merchant1.id)
+      @item2 = Item.create!(name: "Peanut Butter", description: "Yummy", unit_price: 12, merchant_id: @merchant1.id)
+
+      @customer1 = Customer.create!(first_name: "Loki", last_name: "R")
+
+      @invoice1 = @customer1.invoices.create!(status: "completed")
+
+      @ii1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, status: 1, quantity: 20, unit_price: 1000)
+
+      expect(@ii1.discount?).to be(true)
+    end
+    it "returns false if no discount" do
+      @merchant1 = Merchant.create!(name: "Pabu")
+
+      @discount1 = @merchant1.discounts.create!(discount: 0.1, threshold: 5)
+      @discount2 = @merchant1.discounts.create!(discount: 0.4, threshold: 20)
+
+      @item1 = Item.create!(name: "Brush", description: "Brushy", unit_price: 10, merchant_id: @merchant1.id)
+      @item2 = Item.create!(name: "Peanut Butter", description: "Yummy", unit_price: 12, merchant_id: @merchant1.id)
+
+      @customer1 = Customer.create!(first_name: "Loki", last_name: "R")
+
+      @invoice1 = @customer1.invoices.create!(status: "completed")
+
+      @ii1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, status: 1, quantity: 4, unit_price: 1000)
+
+      expect(@ii1.discount?).to be(false)
+    end
   end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -40,5 +40,42 @@ RSpec.describe Invoice, type: :model do
     it "returns all pending_invoices" do
       expect(Invoice.pending_invoices).to eq([@invoice2])
     end
+
+    it "finds the total discount" do
+      @merchant1 = Merchant.create!(name: "Pabu")
+
+      @discount1 = @merchant1.discounts.create!(discount: 0.1, threshold: 5)
+      @discount2 = @merchant1.discounts.create!(discount: 0.5, threshold: 20)
+
+      @item1 = Item.create!(name: "Brush", description: "Brushy", unit_price: 10, merchant_id: @merchant1.id)
+      @item2 = Item.create!(name: "Peanut Butter", description: "Yummy", unit_price: 12, merchant_id: @merchant1.id)
+
+      @customer1 = Customer.create!(first_name: "Loki", last_name: "R")
+
+      @invoice1 = @customer1.invoices.create!(status: "completed")
+
+      @ii1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, status: 1, quantity: 20, unit_price: 1000)
+
+      expect(@invoice1.total_discount).to eq(10000.0)
+    end
+
+    it "finds the difference between total_rev and total_discount" do
+      @merchant1 = Merchant.create!(name: "Pabu")
+
+      @discount1 = @merchant1.discounts.create!(discount: 0.1, threshold: 5)
+      @discount2 = @merchant1.discounts.create!(discount: 0.4, threshold: 20)
+
+      @item1 = Item.create!(name: "Brush", description: "Brushy", unit_price: 10, merchant_id: @merchant1.id)
+      @item2 = Item.create!(name: "Peanut Butter", description: "Yummy", unit_price: 12, merchant_id: @merchant1.id)
+
+      @customer1 = Customer.create!(first_name: "Loki", last_name: "R")
+
+      @invoice1 = @customer1.invoices.create!(status: "completed")
+
+      @ii1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, status: 1, quantity: 20, unit_price: 1000)
+
+      expect(@invoice1.discounted_revenue).to eq(12000.0)
+    end
   end
+
 end
